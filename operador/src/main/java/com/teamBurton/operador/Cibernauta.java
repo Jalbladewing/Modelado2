13fcf68d7ddd270275bfe153b1fe2c42714795bc
abordes96 <abordesgimenez@gmail.com>
2017-04-04 11:02:41 +0200

    inicilazado cibernauta como punto de entrada del server

--- operador/src/main/java/com/teamBurton/operador/Cibernauta.java
+++ operador/src/main/java/com/teamBurton/operador/Cibernauta.java
@@ -1,7 +1,34 @@
 package com.teamBurton.operador;
 
-public class Cibernauta extends Cibernauta_ventana
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+@Theme("newtheme")
+public class Cibernauta extends UI
 {
-	public Cabecera_cibernauta _cabecera;
+	public Cabecera_cibernauta _cabecera = new Cabecera_cibernauta() ;
 	public Contratar_cibernauta _contrata;
+	public Modalidades_destacadas_generico mg = new  Modalidades_destacadas_generico();
+	@Override
+	protected void init(VaadinRequest vaadinRequest) {
+		final VerticalLayout layout = new VerticalLayout();
+         layout.addComponent(_cabecera);
+         layout.addComponent(mg);
+		 setContent(layout);
+	}
+
+	@WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+	@VaadinServletConfiguration(ui = Cibernauta.class, productionMode = false)
+	public static class MyUIServlet extends VaadinServlet {
+	}
+	
 }
--- operador/src/main/java/com/teamBurton/operador/Modalidades_destacadas_generico.java
+++ operador/src/main/java/com/teamBurton/operador/Modalidades_destacadas_generico.java
@@ -6,4 +6,14 @@ public class Modalidades_destacadas_generico extends Modalidades_destacadas_gene
 	public Generico _unnamed_Generico_;
 	public Vector<Modalidad_servicio> _modalidad = new Vector<Modalidad_servicio>();
 	public Vector<Modalidad_oferta_generico> _oferta = new Vector<Modalidad_oferta_generico>();
+
+	public Modalidades_destacadas_generico(){
+		Inicializar();
+		
+	}
+	
+	void Inicializar(){
+		
+		
+	}
 }
