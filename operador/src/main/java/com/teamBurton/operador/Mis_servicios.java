0dc020656d8f856d8bcd59f57d1bc5fcee20f1ea
abordes96 <abordesgimenez@gmail.com>
2017-04-12 19:51:23 +0200

    Retocado y puesto ejemplos en ventanas de clientes
    Falta:
    - ajustar tamaños de elementos de perfil
    - espaciado horizontal en el grid de misServicios
    - navegabilidad a factura
    - ventana emergente de desconectar
    - arreglar desplegable de miCuenta

--- operador/src/main/java/com/teamBurton/operador/Mis_servicios.java
+++ operador/src/main/java/com/teamBurton/operador/Mis_servicios.java
@@ -4,6 +4,7 @@ import java.util.Vector;
 
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.ui.Button;
 
 public class Mis_servicios extends Mis_servicios_ventana implements View {
 	//private Label _seccion;
@@ -11,7 +12,31 @@ public class Mis_servicios extends Mis_servicios_ventana implements View {
 	public Vector<Servicio> _servicio = new Vector<Servicio>();
 	public static final String VIEW_NAME = "mis_servicios";
 	
-	
+	public Mis_servicios(){
+		Servicio s = new Servicio();
+		Button oferta = new Button("Llamadas baratas + 100MB");
+		Button modalidad = new Button("Amigos gratis");
+		oferta.addStyleName("link");
+		modalidad.addStyleName("link");
+		 s.vLayoutModalidadesB.addComponent(oferta);
+		 s.vLayoutModalidadesB.addComponent(modalidad);
+		 s.noServiciosL.setVisible(false);
+		 serviciosGrid.addComponent(s);
+		 
+		 s = new Servicio();
+		 s.tituloL.setValue("Televisión");
+		 serviciosGrid.addComponent(s);
+		 
+		 s = new Servicio();
+		 s.tituloL.setValue("Internet");
+		 serviciosGrid.addComponent(s);
+		
+		 s = new Servicio();
+		 s.tituloL.setValue("Móvil");
+		 serviciosGrid.addComponent(s);
+		
+		
+	}
 	@Override
 	public void enter(ViewChangeEvent event) {
 		// TODO Auto-generated method stub
--- operador/src/main/java/com/teamBurton/operador/Perfil_cliente.java
+++ operador/src/main/java/com/teamBurton/operador/Perfil_cliente.java
@@ -4,6 +4,7 @@ import java.util.Vector;
 
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.ui.Label;
 
 public class Perfil_cliente extends Perfil_cliente_ventana implements View {
 	/*private Label _seccion;
@@ -13,7 +14,38 @@ public class Perfil_cliente extends Perfil_cliente_ventana implements View {
 	public Vector<Datos_no_modificables> _datosNoModificables = new Vector<Datos_no_modificables>();
 	public static final String VIEW_NAME = "perfil_cliente";
 	
-	
+	public Perfil_cliente(){
+		Datos_modificables dm = new Datos_modificables();
+		dm.tituloL.setValue("Nombre");
+		dm.valorL.setValue("Aitor");
+		vLayoutCamposModificables.addComponent(dm);
+		dm = new Datos_modificables();
+		dm.tituloL.setValue("Apellidos");
+		dm.valorL.setValue("Tilla Santos");
+		vLayoutCamposModificables.addComponent(dm);
+		dm = new Datos_modificables();
+		dm.tituloL.setValue("Dirección");
+		dm.valorL.setValue("C/America nº7, 2ºA");
+		vLayoutCamposModificables.addComponent(dm);
+		dm = new Datos_modificables();
+		dm.tituloL.setValue("Telefono");
+		dm.valorL.setValue("950000000");
+		vLayoutCamposModificables.addComponent(dm);
+		
+		Datos_no_modificables dn = new Datos_no_modificables();
+		dn.tituloL.setValue("E-mail");
+		dn.valorL.setValue("tilla96@gmail.com");
+		vLayoutDatosNoModificables.addComponent(dn);
+		dn = new Datos_no_modificables();
+		dn.tituloL.setValue("DNI/NIF/Pasaporte");
+		dn.valorL.setValue("9999999999");
+		vLayoutDatosNoModificables.addComponent(dn);
+		dn = new Datos_no_modificables();
+		dn.tituloL.setValue("Contraseña");
+		dn.valorL.setValue("*******");
+		vLayoutDatosNoModificables.addComponent(dn);
+		
+	}
 	@Override
 	public void enter(ViewChangeEvent event) {
 		// TODO Auto-generated method stub
