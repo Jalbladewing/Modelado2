7dcaa829bc4d19cb8241dc7115f3e667ea8d8ac8
Jalbladewing <p.guillen100@gmail.com>
2017-05-17 13:52:45 +0200

    BD de administrador y canal terminados excepto generar password.

--- operador/src/main/java/bd/BD_Administrador.java
+++ operador/src/main/java/bd/BD_Administrador.java
@@ -1,29 +1,80 @@
 package bd;
 
+import java.util.Arrays;
 import java.util.List;
 
 import java.util.Vector;
 
 import org.orm.PersistentException;
+import org.orm.PersistentTransaction;
 
 
 public class BD_Administrador {
 	public BD_Principal _bd_prin_admin;
 	public Vector<Administrador> _contiene_admin = new Vector<Administrador>();
 
-	public List cargar_modalidades_destacadas()  throws PersistentException {
+	public List cargar_modalidades_destacadas()  throws PersistentException 
+	{
+		List<Modalidad> modalidades = null;
+		List<Administrador> administrador = null;
+		PersistentTransaction t = bd.IteracionFinalPersistentManager.instance().getSession().beginTransaction();
+		
+		try
+		{
+			administrador = AdministradorDAO.queryAdministrador(null, null);
+			modalidades = Arrays.asList(administrador.get(0).modalidadDestacada.toArray());
+			
+			t.commit();
+			
+		}catch(Exception e)
+		{
+			t.rollback();
+		}
+		
+		return modalidades;
+	}
+
+	public boolean acceder_administrador(int idAdministrador) throws PersistentException  
+	{
+		Administrador administrador = null;
+		boolean resultado = false;
+		PersistentTransaction t = bd.IteracionFinalPersistentManager.instance().getSession().beginTransaction();
+		
+		try
+		{
+			administrador = AdministradorDAO.getAdministradorByORMID(idAdministrador);
+			resultado = true;
+			t.commit();
+			
+		}catch(Exception e)
+		{
+			t.rollback();
+		}
+		
+		return resultado;
+	}
+
+	public String generar_password(int aIdAdministrador) throws PersistentException  
+	{
 		throw new UnsupportedOperationException();
 	}
 
-	public boolean acceder_administrador(int aIdAdministrador) {
-		throw new UnsupportedOperationException();
-	}
-
-	public String generar_password(int aIdAdministrador) {
-		throw new UnsupportedOperationException();
-	}
-
-	public boolean modificar_perfil_administrador(Administrador aAdministrador) {
-		throw new UnsupportedOperationException();
+	public boolean modificar_perfil_administrador(Administrador administrador) throws PersistentException  
+	{
+		boolean resultado = false;
+		PersistentTransaction t = bd.IteracionFinalPersistentManager.instance().getSession().beginTransaction();
+		
+		try
+		{
+			resultado = AdministradorDAO.refresh(administrador);
+			
+			t.commit();
+			
+		}catch(Exception e)
+		{
+			t.rollback();
+		}
+		
+		return resultado;
 	}
 }
--- operador/src/main/java/bd/BD_Canal.java
+++ operador/src/main/java/bd/BD_Canal.java
@@ -1,7 +1,11 @@
 package bd;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Vector;
+
+import org.orm.PersistentException;
+import org.orm.PersistentTransaction;
 
 
 
@@ -9,19 +13,83 @@ public class BD_Canal {
 	public BD_Principal _bd_prin_canal;
 	public Vector<Canal> _contiene_canal = new Vector<Canal>();
 
-	public List cargar_canales() {
-		throw new UnsupportedOperationException();
+	public List cargar_canales()  throws PersistentException 
+	{
+		List<Canal> canales = null;
+		PersistentTransaction t = bd.IteracionFinalPersistentManager.instance().getSession().beginTransaction();
+		
+		try
+		{
+			canales = CanalDAO.queryCanal(null, null);
+			
+			t.commit();
+			
+		}catch(Exception e)
+		{
+			t.rollback();
+		}
+		
+		return canales;
 	}
 
-	public List cargar_modalidades_factura(int aIdFactura) {
-		throw new UnsupportedOperationException();
+	public List cargar_modalidades_factura(int idFactura)  throws PersistentException 
+	{
+		List<Modalidad> modalidades = null;
+		Factura factura = null;
+		PersistentTransaction t = bd.IteracionFinalPersistentManager.instance().getSession().beginTransaction();
+		
+		try
+		{
+			factura = FacturaDAO.getFacturaByORMID(idFactura);
+			modalidades = Arrays.asList(factura.modalidad.toArray());
+			
+			t.commit();
+			
+		}catch(Exception e)
+		{
+			t.rollback();
+		}
+		
+		return modalidades;
 	}
 
-	public List cargar_modalidades_cliente(int idCliente) {
-		throw new UnsupportedOperationException();
+	public List cargar_modalidades_cliente(int idCliente)  throws PersistentException 
+	{
+		List<Modalidad> modalidades = null;
+		Cliente cliente = null;
+		PersistentTransaction t = bd.IteracionFinalPersistentManager.instance().getSession().beginTransaction();
+		
+		try
+		{
+			cliente = ClienteDAO.getClienteByORMID(idCliente);
+			modalidades = Arrays.asList(cliente.getModalidads());
+			
+			t.commit();
+			
+		}catch(Exception e)
+		{
+			t.rollback();
+		}
+		
+		return modalidades;
 	}
 
-	public boolean crear_modalidad_canal(Canal aCanal) {
-		throw new UnsupportedOperationException();
+	public boolean crear_modalidad_canal(Canal canal)  throws PersistentException 
+	{
+		boolean resultado = false;
+		PersistentTransaction t = bd.IteracionFinalPersistentManager.instance().getSession().beginTransaction();
+		
+		try
+		{
+			resultado = CanalDAO.save(canal);
+			
+			t.commit();
+			
+		}catch(Exception e)
+		{
+			t.rollback();
+		}
+		
+		return resultado;
 	}
 }
