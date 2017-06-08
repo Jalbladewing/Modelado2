e00f9ff165cc489e7943545eed84e99f2856f290
abordes96 <abordesgimenez@gmail.com>
2017-06-08 18:36:12 +0200

    Gestor de correos implementado

--- operador/src/main/java/com/teamBurton/operador/Gestor_Correos.java
+++ operador/src/main/java/com/teamBurton/operador/Gestor_Correos.java
@@ -1,52 +1,71 @@
 package com.teamBurton.operador;
+
 import java.util.Properties;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
-public class Gestor_Correos {
-	
-private final Properties properties = new Properties();
-	
+public class Gestor_Correos implements Gestor_correo {
+
+	private final Properties properties = new Properties();
+
 	private String password;
- 
+
 	private Session session;
- 
+
 	private void init() {
- 
-		properties.put("mail.smtp.host", "mail.gmail.com");
-		properties.put("mail.smtp.starttls.enable", "true");
-		properties.put("mail.smtp.port",25);
-		properties.put("mail.smtp.mail.sender","emisor@gmail.com");
-		properties.put("mail.smtp.user", "usuario");
-		properties.put("mail.smtp.auth", "true");
- 
-		session = Session.getDefaultInstance(properties);
+
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.stmp.user", "username");
+
+		/*
+		 * //To use TLS props.put("mail.smtp.auth", "true");
+		 * props.put("mail.smtp.starttls.enable", "true");
+		 * props.put("mail.smtp.password", "opticometria");
+		 */
+
+		// To use SSL
+		props.put("mail.smtp.socketFactory.port", "465");
+		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.port", "465");
+
+		session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication("teamburton96@gmail.com", "opticometria");// Specify
+			}
+		});
 	}
- 
-	public void sendEmail(){
- 
+
+	public Boolean enviar_correo_recuperacion(String email, String password) {
 		init();
-		try{
-			MimeMessage message = new MimeMessage(session);
-			message.setFrom(new InternetAddress((String)properties.get("mail.smtp.mail.sender")));
-			message.addRecipient(Message.RecipientType.TO, new InternetAddress("receptor@gmail.com"));
-			message.setSubject("Prueba");
-			message.setText("Texto");
-			Transport t = session.getTransport("smtp");
-			t.connect((String)properties.get("mail.smtp.user"), "password");
-			t.sendMessage(message, message.getAllRecipients());
-			t.close();
-		}catch (MessagingException me){
-                        //Aqui se deberia o mostrar un mensaje de error o en lugar
-                        //de no hacer nada con la excepcion, lanzarla para que el modulo
-                        //superior la capture y avise al usuario con un popup, por ejemplo.
-			return;
-		}
-		
-	}
- 
+		String from = "teamburton96@gmail.com";
+        String subject = "Recuperacion de contraseña";
+        Message msg = new MimeMessage(session);
+        try {
+            msg.setFrom(new InternetAddress(from));
+            msg.setRecipient(Message.RecipientType.TO, 
+                new InternetAddress(email));
+            msg.setSubject(subject);
+            msg.setText(" Hola " + email + ", \n \n Hemos recibido una petición de "
+            		+ "recuperación de contraseña para su cuenta, "
+            		+ "su contraseña actual es " + password +"\n\n \n Si no ha solicitado"
+            				+ " recuperar su contraseña, "
+            				+ "puede ponerse en contacto con nuestro soporte técnico teamburton96@gmail.com.");
+            Transport transport = session.getTransport("smtp");
+            transport.connect("smtp.gmail.com" , 465 , "username", "password");
+            transport.send(msg);
+            return true;
+        }
+        catch(Exception exc) {
+            System.out.println(exc);
+            return false;
+        }
+        
+    }
 }
--- operador/src/main/java/com/teamBurton/operador/Gestor_correo.java
+++ operador/src/main/java/com/teamBurton/operador/Gestor_correo.java
@@ -1,4 +1,6 @@
 package com.teamBurton.operador;
 
 public interface Gestor_correo {
+	
+	public Boolean enviar_correo_recuperacion(String email, String password);
 }
